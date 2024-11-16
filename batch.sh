6900581a081df5868a8861661cf250f297cc691e
dumbasPL <29180158+dumbasPL@users.noreply.github.com>
2024-11-16 21:47:14 +0100

    add batch script

---
+++ batch.sh
@@ -0,0 +1,26 @@
+#!/usr/bin/env bash
+
+set -e -o pipefail
+
+if [ -z "$GITHUB_TOKEN" ]; then
+    echo "GITHUB_TOKEN is not set"
+    exit 1
+fi
+
+while IFS= read -r line; do
+    # create github action body using jq
+    data=$(jq -n -c --arg manifestId "$line" '{ref: "master", inputs: {depotId: "2347771", manifestId: $manifestId, gitBranch: "test2"}}')
+
+    echo "Triggering github action for manifest $line"
+
+    # # trigger github action
+    curl -X POST \
+        -H "Accept: application/vnd.github+json" \
+        -H "Authorization: Bearer $GITHUB_TOKEN" \
+        -H "X-GitHub-Api-Version: 2022-11-28" \
+        https://api.github.com/repos/cs2-analysis/cs2-dex/actions/workflows/update.yml/dispatches \
+        -d "$data"
+    
+    echo "$line" >> triggered.txt
+    sleep 120
+done < manifests.txt
